1cd7eefcaa6d0b03d853eee940e0f6806eb6106d
柴米油盐 <471097828@qq.com>
2017-12-28 18:52:05 +0800

    prop的数据类型检测type；不同数据类型传递，子组件改变数据对父组件的影响不同；同步情况下

--- src/components/PsendmsgtoC.vue
+++ src/components/PsendmsgtoC.vue
@@ -5,20 +5,31 @@
 		<p>单向数据流动是vue推荐的，就是父组件改变影响子组件，反之则不影响一般用prop座位桥梁</p>
 		<h1>这是父组件</h1>
 		<p>input框是用来改变父组件数据的看看对子组件是否产生影响</p>
-		<input type="text" name="" v-model='Pmsg'>
+		<input type="text" name="" v-model='msg'>
 		<!-- 利用v-bind:子组件数据名=‘父组件要发送的数据’ -->
-		<callChildOne :msg="Pmsg"></callChildOne>
+		<p>子组件1</p>
+		<callChildOne :msg="msg"></callChildOne>
+		<h2>数组类型的数据msg3</h2>
+		<ul>
+			<li v-for='(list,index) in msg3'>{{index}}====={{list}}</li>
+		</ul>	
+		<p>子组件2</p>
+		<callChildTwo :msg-string='msg1' :msg-array='msg3'></callChildTwo>
 	</div>
 </template>
 <script>
-	import callChildOne from './child/callChild.vue'
+	import callChildOne from './child/callChild.vue';
+	import callChildTwo from './child/callchildtwo.vue';
 	export default {
 		name: 'PsendC',
 		data() {
 			return {
-				Pmsg: '--父组件的数据--'
+				msg: '--父组件的数据--',
+				msg1: '父组件的数据',
+				msg2: '1',
+				msg3: ['one','two','tree']
 			}
 		},
-		components: {callChildOne}
+		components: {callChildOne,callChildTwo}
 	}
 </script>
